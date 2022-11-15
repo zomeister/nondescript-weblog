241e4b09e2c18b484f112e8458d98b224d067160
Zoe Guidry <oyezoey@gmail.com>
2022-11-14 23:03:00 -0600

    figured out imports for react, react-bootstrap, and react-three

---
+++ weblogapp/src/App.tsx
@@ -0,0 +1,40 @@
+import React, {useState, Component} from 'react';
+import Container from 'react-bootstrap/Container';
+// import logo from './logo.svg';
+import './App.css';
+/** import Components from `./components/` */
+// import Header from './components/Header.js'
+import Article from './components/Article.js'
+// import Footer from './components/Footer.js'
+// import Main from './components/Main.js'
+
+
+// type AppProps = {
+//   website: string
+//   count: number
+// }
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <p>hello</p>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      {/* <Article /> */}
+    </div>
+  );
+}
+
+export default App
---
+++ weblogapp/src/components/Article.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+// /**import 'Article.css' or 'Article.scss' */
+// // import NavBar f
+// import NavBarEx from './NavBarEx.js'
+
+
+
+const Article = (props)=>{ return (
+    <article className='weblogpost'>
+        <hgroup className='weblog-id'>
+tgg
+        </hgroup>
+        <hr id='content'/>
+        <section>jdj fdksj  djfkjs jdfkds djfks kdjfks kdskfjks dfksjfks djkfs jdks fjsdkl </section>
+
+    </article>
+)} 
+
+export default Article
